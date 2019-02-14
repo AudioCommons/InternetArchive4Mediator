21338fefd0f62d084d5db35e5d37a615eb6782b7
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2019-02-14 17:58:10 +0000

    Added new sections to sparqlgenerate.sparql

--- sparqlgen.sh
+++ sparqlgen.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # output redirection with '> out.txt'
-curl -v -H POST "http://localhost:8331/transform" --data-urlencode query@./sparqlgenerate.sparql
+curl -v -H POST "http://localhost:8331/transform" --data-urlencode query@$1
